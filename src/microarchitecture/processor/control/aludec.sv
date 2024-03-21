5b0eb5c917b72ebb4b0895bb9d31a0d516bb5540
Simón Fallas <47420204+SimonFV@users.noreply.github.com>
2024-03-20 20:07:10 -0600

    Alu control signals

--- src/microarchitecture/processor/control/aludec.sv
+++ src/microarchitecture/processor/control/aludec.sv
@@ -11,11 +11,15 @@ module aludec
 			2'b00: alucontrol = 3'b010; // add
 			2'b01: alucontrol = 3'b110; // sub
 			default: case(funct) // RTYPE
-							6'b100000: alucontrol = 3'b010; // ADD
-							6'b100010: alucontrol = 3'b110; // SUB
-							6'b100100: alucontrol = 3'b000; // AND
-							6'b100101: alucontrol = 3'b001; // OR
-							6'b101010: alucontrol = 3'b111; // SLT
+							6'b000000: alucontrol = 3'b010; // add
+							6'b000001: alucontrol = 3'b110; // sub
+							6'b000100: alucontrol = 3'b010; // add.fp
+							6'b000110: alucontrol = 3'b000; // mul.fp
+							6'b100100: alucontrol = 3'b010; // vadd.fp
+							6'b100110: alucontrol = 3'b000; // vmul.fp
+							6'b110000: alucontrol = 3'b011; // vsum.fp
+							6'b100101: alucontrol = 3'b111; // vset.fp
+							
 							default: alucontrol = 3'bxxx; // ???
 						endcase
 		endcase
