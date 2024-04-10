8501a3c63060c3792260173b7c23d930b8bf223d
04esteban04 <estebanmadrigal7@gmail.com>
2024-04-10 03:15:19 -0600

    Generation of .txt data files and FIR filtering function

--- src/input signal/input signal generation.py
+++ src/input signal/input signal generation.py
@@ -27,13 +27,19 @@ def convert_toQ78(signal):
 def convert_Q78_toHex(signal):
     return np.round(signal * (2**8)).astype(np.int16) / (2**8)
 
-# Function to print the signal values tabulated
-def print_data(time, signal, signal_q78, signal_Hex):
-    print("\n*** INPUT SIGNAL ***\n")
-    print("Time (s) \t Signal Data \t\t Q7.8 Data \t\t Hex Data")
-    for t, value, value_Q78, value_Q78_toHex in zip(time, signal, signal_q78, signal_Hex):
-        hex_value = float_to_q78_hex(value_Q78_toHex)
-        print(f"{t:.2f} \t\t {value:.8f} \t\t {value_Q78:.8f} \t\t {hex_value}")
+# Function to write the signal values tabulated to a file
+def write_signal_data_to_file(time, signal, signal_q78, signal_Hex, file_name):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    os.makedirs(current_directory, exist_ok=True)
+
+    with open(os.path.join(current_directory, file_name), 'w') as file:
+        file.write("*** INPUT SIGNAL ***\n\n")
+        file.write("Time (s) \t Signal Data \t\t Q7.8 Data \t\t Hex Data\n")
+        for t, value, value_Q78, value_Q78_toHex in zip(time, signal, signal_q78, signal_Hex):
+            hex_value = float_to_q78_hex(value_Q78_toHex)
+            file.write(f"{t:.2f} \t\t {value:.8f} \t\t {value_Q78:.8f} \t\t {hex_value}\n")
+    
+    print("\nThe input signal file was created successfully.\n")
 
 # Funtion to plot signal
 def plot_signal(x_signal, y_signal):
@@ -46,7 +52,7 @@ def plot_signal(x_signal, y_signal):
 period = 5
 samples = 39200 * 5
 time = np.linspace(0, period, samples, endpoint=False)
-frequency = 1
+frequency = 1000
 sine_wave = create_sine_wave(time, frequency)
 
 # Convert to Q7.8 and Hex
@@ -54,10 +60,10 @@ sine_q78 = convert_toQ78(sine_wave)
 sine_q78_Hex = convert_Q78_toHex(sine_wave)
 
 # Print data
-print_data(time, sine_wave, sine_q78, sine_q78_Hex)
+write_signal_data_to_file(time, sine_wave, sine_q78, sine_q78_Hex, "input_signal_data.txt")
 
 # Plot
-#plot_signal(time, sine_wave)
+plot_signal(time, sine_wave)
 
 
 
@@ -65,9 +71,15 @@ print_data(time, sine_wave, sine_q78, sine_q78_Hex)
         COEFFICIENT GENERATION
 """""""""""""""""""""""""""""""""""
 
+# Function to adjust the filter coefficients
+def adjust_coefficients(coefficients):
+    epsilon = 1e-6  # Valor pequeño para evitar división por cero
+    adjusted_coefficients = np.where(np.logical_or(coefficients <= -0.99 + epsilon, coefficients >= 0.99 - epsilon),
+                                      np.sign(coefficients) * 0.99,
+                                      coefficients)
+    return adjusted_coefficients
+
 # Function to generate the filter coefficients
-#   frequency = filter cutoff frequency
-#   filter_order = filter order (number of coefficients)    
 def create_coefficients(frequency, filter_order):
     filter_coefficients = firwin(filter_order + 1, frequency)
     max_abs_coefficient = max(abs(filter_coefficients))
@@ -75,18 +87,25 @@ def create_coefficients(frequency, filter_order):
 
     if max(abs(normalized_coefficients)) > 1:
         normalized_coefficients /= max(abs(normalized_coefficients))
-
+    
+    normalized_coefficients = adjust_coefficients(normalized_coefficients)
     normalized_coefficients = np.round(normalized_coefficients, 8)
 
     return normalized_coefficients
 
-# Function to print the coefficients values tabulated
-def print_data(signal, signal_q78, signal_Hex):
-    print("\n*** COEFFICIENTS ***\n")
-    print("Coefficients Data \t Q7.8 Data \t\t Hex Data")
-    for value, value_Q78, value_Q78_toHex in zip(signal, signal_q78, signal_Hex):
-        hex_value = float_to_q78_hex(value_Q78_toHex)
-        print(f"{value:.8f} \t\t {value_Q78:.8f} \t\t {hex_value}")
+# Function to write the coefficients values tabulated to a file
+def write_coefficients_data_to_file(signal, signal_q78, signal_Hex, file_name):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    os.makedirs(current_directory, exist_ok=True)
+
+    with open(os.path.join(current_directory, file_name), 'w') as file:
+        file.write("*** COEFFICIENTS ***\n\n")
+        file.write("Coefficients Data \t\t Q7.8 Data \t\t Hex Data\n")
+        for value, value_Q78, value_Q78_toHex in zip(signal, signal_q78, signal_Hex):
+            hex_value = float_to_q78_hex(value_Q78_toHex)
+            file.write(f"{value:.8f} \t\t\t {value_Q78:.8f} \t\t {hex_value}\n")
+    
+    print("The coefficients file was created successfully.\n")
 
 # Generate coefficients
 frequency = 0.1
@@ -96,7 +115,65 @@ coefficients = create_coefficients(frequency, filter_order)
 coefficients_q78 = convert_toQ78(coefficients)
 coefficients_q78_Hex = convert_Q78_toHex(coefficients_q78)
 
-print_data(coefficients, coefficients_q78, coefficients_q78_Hex)
+write_coefficients_data_to_file(coefficients, coefficients_q78, coefficients_q78_Hex, "coefficients_data.txt")
+
+
+
+"""""""""""""""""""""""""""""""""""
+            FIR FILTER 
+"""""""""""""""""""""""""""""""""""
+
+# Function to calculate the scalar FIR filter
+# diferrence equation
+def fir_filter(x, coefficients):
+    coefficients_len = len(coefficients)
+    x_len = len(x)
+    y = []
+
+    for i in range(x_len):
+        y.append(0)
+        for j in range(coefficients_len):
+            if i - j >= 0:
+                y[i] += coefficients[j] * x[i - j]
+
+    return y
+
+# Function to calculate the scalar FIR filter
+# sum equation
+def fir_filter_sum_eq(x, coefficients):
+    y = []
+
+    for i in range(len(x)):
+        y.append(0)
+        for j in range(len(coefficients)):
+            if i + j < len(x):
+                y[i] += x[i + j] * coefficients[j]
+
+    return y
+
+# Function to write the filter results to a file
+def write_filter_results_to_file(signal_q78, file_name):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    os.makedirs(current_directory, exist_ok=True)
+
+    with open(os.path.join(current_directory, file_name), 'w') as file:
+        file.write("*** FIR FILTER RESULTS ***\n\n")
+        file.write("Q7.8 Data\n")
+        for value_Q78 in signal_q78:
+            file.write(f"{value_Q78:.8f}\n")
+    
+    print("The fir_filter_results file was created successfully.\n")
+
+x = sine_q78
+bk = coefficients_q78
+
+y = fir_filter(x, bk)
+y2 = fir_filter_sum_eq(x, bk)
+
+# Write the results to the file
+write_filter_results_to_file(y, "fir_filter_data.txt")
+write_filter_results_to_file(y2, "fir_filter_sum_eq_data.txt")
+
 
 
 """""""""""""""""""""""""""""""""""
@@ -138,6 +215,9 @@ def create_mif_file(depth, width, sine_q78_Hex, coefficients_q78_Hex):
         
         hex_values_concatenated = "" 
         
+        mif_file.write(f"\t{pos} : {'0'*64};\n")
+        pos += 1
+
         # Write the coefficients data
         for value_Q78_Hex in coefficients_q78_Hex:
             hex_value = float_to_q78_hex(value_Q78_Hex)
@@ -158,7 +238,7 @@ def create_mif_file(depth, width, sine_q78_Hex, coefficients_q78_Hex):
         # End the content of the .mif file
         mif_file.write("END;")
 
-    print("\nThe .mif file was created successfully.\n")
+    print("The .mif file was created successfully.\n")
 
 # Create .mif file
 depth = 12288  
@@ -168,5 +248,6 @@ create_mif_file(depth, width, sine_q78_Hex, coefficients_q78_Hex)
 
 # LINE 0                : RESERVED
 # LINE 1 - 12250        : AUDIO FILE
-# LINE 12251 - 12252    : COEFFICIENTS
-# LINE 12252 - 12287    : EMPY SPACE
+# LINE 12251            : RESERVED
+# LINE 12252 - 12253    : COEFFICIENTS
+# LINE 12254 - 12287    : EMPY SPACE
